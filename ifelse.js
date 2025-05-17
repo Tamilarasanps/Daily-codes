8a01719ae43086b2473e09a0cc729c5c7a4cee65
Tamilarasanps <tamiltamilarasan2530@gmail.com>
2025-05-17 17:08:06 +0530

    ifelse

---
+++ ifelse.js
@@ -0,0 +1,27 @@
+//1. odd or even
+
+// let value1 = 7;
+// if (value1 %2==0) {
+//   console.log(value1, 'even');
+// } else {
+//   console.log("odd", value1);
+// }
+
+//2: Grade Calculator
+
+// let mark = 80;
+// if (mark >= 90 && mark <= 100) {
+//   console.log("A grade :", mark);
+// } else if (mark >= 80 && mark <= 89) {
+//   console.log("B grade :", mark);
+// } else if (mark >= 70 && mark <= 79) {
+//   console.log("C grade :", mark);
+// } else if (mark >= 60 && mark <= 69) {
+//   console.log("d grade :", mark);
+// } else {
+//   console.log("fail");
+// }
+
+//3: Leap Year Checker
+
+
